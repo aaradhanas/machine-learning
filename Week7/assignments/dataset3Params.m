22a4f591a476dd795ac3d0d0a88c7699f57a7fb2
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-04-06 08:00:44 +0530

    Week 7 assign part 2

--- Week7/assignments/dataset3Params.m
+++ Week7/assignments/dataset3Params.m
@@ -24,9 +24,39 @@ sigma = 0.3;
 %
 
 
+error_matrix = zeros(64, 3);
+k = 1;
+% C values
+C_vector = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+% sigma values
+sigma_vector = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+%for i = 1: size(C_vector, 1)
+%  for j = 1: size(sigma_vector, 1)
+%    model = svmTrain(X, y, C_vector(i) , @(x1, x2) gaussianKernel(x1, x2, sigma_vector(j)));
+%    prediction = svmPredict(model, Xval);
+%    pred_error = mean(double(prediction ~= yval));
+%    error_matrix(k,1) = C_vector(i);
+%    error_matrix(k,2) = sigma_vector(j);
+%    error_matrix(k,3) = pred_error;
+%    k++;
+%  end
+%end 
 
+%for i = 1: size(error_matrix, 1)
+%  fprintf('pred_error for C=%f, sigma=%f is %f', error_matrix(i,1), error_matrix(i,2), error_matrix(i,3));
+%end
+
+%[min, index] = min(error_matrix(:,3));
+
+%C = error_matrix(index, 1);
+%sigma = error_matrix(index, 2);
+
+C = 1.000000;
+sigma = 0.100000;
+
+%fprintf('min error for C=%f, sigma=%f is %f',C, sigma, min);
 
 % =========================================================================
 
